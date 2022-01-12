f0a3c37b1fec3f9b84d72e6f467004e6f09c0e0d
yani <54736274+YaniPrieto@users.noreply.github.com>
2022-01-12 15:57:32 +0800

    Add files via upload

---
+++ Admin/assets/js/firebaseConfig.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyAYndVLtinEJqSjbOK8XcCwny18g0S_H9U",
+    authDomain: "test-assessment-c4e1c.firebaseapp.com",
+    projectId: "test-assessment-c4e1c",
+    storageBucket: "test-assessment-c4e1c.appspot.com",
+    messagingSenderId: "304160475144",
+    appId: "1:304160475144:web:0b904b87320e8bd26ee9bd"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
---
+++ vendor/bootstrap/js/firebaseConfig.js
@@ -0,0 +1,11 @@
+
+import { initializeApp } from "firebase/app";
+const firebaseConfig = {
+    apiKey: "AIzaSyAYndVLtinEJqSjbOK8XcCwny18g0S_H9U",
+    authDomain: "test-assessment-c4e1c.firebaseapp.com",
+    projectId: "test-assessment-c4e1c",
+    storageBucket: "test-assessment-c4e1c.appspot.com",
+    messagingSenderId: "304160475144",
+    appId: "1:304160475144:web:0b904b87320e8bd26ee9bd"
+};
+const app = initializeApp(firebaseConfig);
